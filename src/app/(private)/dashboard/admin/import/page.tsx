aa5e9bd68ffe7574ca93687f50d450c23d3344f6
Nico Barnuevo <nicoleyro13@gmail.com>
2025-08-07 23:54:19 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(private)/dashboard/admin/import/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-
-// This page has been removed as per user request.
-export default function ImportPage() {
-    return null;
-}
--- src/app/dashboard/admin/import/page.tsx
+++
@@ -1,2 +0,0 @@
-// This file was intentionally left blank as part of the revert.
-// The CSV import functionality will be reimplemented based on the original, stable code.
